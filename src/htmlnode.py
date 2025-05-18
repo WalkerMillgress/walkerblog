37803f07cd9f60ace1fd3b8e11edfeea82b16456
Walker M. <walker@1ks.ink>
2025-05-18 10:49:37 -0300

    Now with parents!

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,3 +25,15 @@ class LeafNode(HTMLNode):
             return self.value
         else:
             return f"<{self.tag}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("There is no tag, you doofus!")
+        elif self.children == None:
+            raise ValueError("There are no children! How!")
+        else:
+            return f"<{self.tag}>{"".join(list(map(lambda x: x.to_html(), self.children)))}</{self.tag}>"
