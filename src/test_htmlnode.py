b10b2d02fcfcd4011df74172f5b3c6a5600f450b
Walker M. <walker@1ks.ink>
2025-05-21 12:31:41 -0300

    Doing a bit too much.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,7 +2,7 @@ import unittest
 
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, TextType
-from converters import text_node_to_html_node
+from converters import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, text_to_textnodes
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -37,6 +37,35 @@ class TestHTMLNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    def test_delimiter(self):
+        node = TextNode("This is a text node with _emphasis_ and *bold*.", TextType.NORMAL)
+        html_node = split_nodes_delimiter([node], "*", TextType.BOLD)
+        print(html_node)
+        html_node = split_nodes_delimiter(html_node, "_", TextType.ITALIC)
+        print(html_node)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+    def test_text_to_nodes(self):
+        nodes = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        self.assertListEqual([
+            TextNode("This is ", TextType.NORMAL),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.NORMAL),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.NORMAL),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.NORMAL),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.NORMAL),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ], nodes)
 
 if __name__ == "__main__":
     unittest.main()
